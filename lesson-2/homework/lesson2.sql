55cc6889c5447699ff4bd17468da4547c71d852a
foryumohi17 <m64752682@gmail.com>
2025-03-05 18:03:36 +0500

    Create lesson2.sql

---
+++ lesson-2/homework/lesson2.sql
@@ -0,0 +1,12 @@
+
+-- 3. Create Orders Table
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerID INT,
+    ProductID INT,
+    OrderDate DATE,
+    Quantity INT,
+    TotalAmount DECIMAL(10, 2),
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID),
+    FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+);
